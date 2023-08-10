23a1aabcc9dbfe8cf51b945c353a48a6a6eb71a3
00aryan <aryankumar6203832171@gmail.com>
2023-08-09 18:47:30 -0800

    new add

---
+++ python/ind.py
@@ -0,0 +1,18 @@
+# 1.open sourse interpretator server side scripting language
+# 2.easy to learn
+# 3.time complixity/space complixity
+# 4.it is imbeded in any language
+# 5.platform independent(like ai,robotic,machine learning,iot)
+# 6.data analysis fast
+# 7.use in nasa, google, gov.system
+# 8.follow oops concept
+# 9.more secure
+# 10.processor high
+# variable
+# 1. variable is two type
+# a.user define variable
+# b.predefined variavle
+#   1.premetive data type(example int a=5,int,str,float,double,boolen)
+#   2.non-premetive data type (example array, list,dictionary,tuple,set)
+2. local variable
+  
